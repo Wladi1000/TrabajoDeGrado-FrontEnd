464bb5d7fa5724d96b77f710af4814da1acd0a4e
Wladimir <wladimirss14@gmail.com>
2023-06-16 18:49:28 -0400

    Maquetado de ventana admin

--- src/views/AdminOverview.vue
+++ src/views/AdminOverview.vue
@@ -2,6 +2,18 @@
 </script>
 <template>
   <div class="overview">
-    <h2>Overview Page</h2>
+    <div class="container h-100">
+      <div class="intro h-100">
+        <div class="row h-100 justifu-content-center align-items-center">
+          <div class="col-md-6">
+            <h3>Pagina Over View</h3>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui quo veniam architecto, minima harum est? Ex est voluptatum aliquam temporibus eveniet a odio odit quos voluptate provident, nulla expedita nam. </p>
+          </div>
+          <div class="col-md-6">
+            <img src="/img/svg/overview.svg" alt="overview" class="img-fl">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
