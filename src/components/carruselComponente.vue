3b9fdc5bfc90e90b40baefb871312868b970f326
Wladimir <wladimirss14@gmail.com>
2023-06-20 13:01:32 -0400

    Bootstrap con los componentes de productos

--- src/components/carruselComponente.vue
+++ src/components/carruselComponente.vue
@@ -26,14 +26,14 @@
 
 </script>
 <template>
-  <div class="carrusel-componente">
+  <div class="card carrusel-componente" style="width: 14rem;">
     <img :src="imagen || ''" :alt="nombre + ' ' + marca">
-    <div class="carrusel-componente__detalles">
-      <h3 class="carrusel-componente__detalles--precio">{{ precio }}</h3>
-      <p class="carrusel-componente__detalles--nombre">{{ nombre }}</p>
+    <div class="card-body carrusel-componente__detalles">
+      <h3 class="card-title arrusel-componente__detalles--precio">{{ precio }}</h3>
+      <p class="card-text carrusel-componente__detalles--nombre">{{ nombre }}</p>
       <!-- <p class="carrusel-componente__detalles--descripcion">{{ descripcion }}</p> -->
       <!-- <p class="carrusel-componente__detalles--marca">{{ marca }}</p> -->
-      <p class="carrusel-componente__detalles--reputacion">{{ reputacion }}</p>
+      <p class="card-text carrusel-componente__detalles--reputacion">{{ reputacion }}♦♦♦♦♦ </p>
     </div>
   </div>
 </template>
