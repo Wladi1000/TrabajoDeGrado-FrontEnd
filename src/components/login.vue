a799cd5c5054eeb04dfd80f4c742a2db8d995284
Wladimir <wladimirss14@gmail.com>
2023-06-21 20:20:23 -0400

    Fix export firebase

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue';
-//import firebase from 'node_modules/firebase';
+import { firebaseApp } from '../firebaseConfig.js';
 
 let loginForm = ref(true);
 
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,3 +19,15 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+let firebaseApp;
+
+if (getApps().length === 0) {
+  firebaseApp = initializeApp(firebaseConfig);
+} else {
+  console.log("Ya hay una app iniciada")
+  firebaseApp = getApp();
+
+}
+
+export { firebaseApp };
